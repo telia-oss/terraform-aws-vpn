add8ebbd19de649f95742239058b2d73c54c5556
Colin Coleman <colin.coleman@kontrollsmart.com>
2018-07-24 14:14:29 +0200

    added descriptions to outputs

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,11 @@
 # ------------------------------------------------------------------------------
 
 output "tunnel1_address" {
-  value = "${aws_vpn_connection.main.tunnel1_address}"
+  description = "The public IP address of the first VPN tunnel."
+  value       = "${aws_vpn_connection.main.tunnel1_address}"
 }
 
 output "tunnel2_address" {
-  value = "${aws_vpn_connection.main.tunnel2_address}"
+  description = "The public IP address of the second VPN tunnel."
+  value       = "${aws_vpn_connection.main.tunnel2_address}"
 }
